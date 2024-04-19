b429aec0edf46f0451ee5ee6ad78a7b80a7732a1
course.cap6412 Student 5 <kevinqaz45@gmail.com>
2024-04-19 19:28:47 -0400

    update model_builder.py

--- VIMA/model_builder.py
+++ VIMA/model_builder.py
@@ -18,7 +18,7 @@ class ModelBuilder():
             self.model.fc3.bias.data = checkpoint['fc3.bias']
 
         if model_type == "mlp_pad":
-            self.model = MLP_Pad(input_size=30*1*768, hidden_size=269, output_size=30*1*768, batch=32)
+            self.model = MLP_Pad(input_size=30*1*768, hidden_size=269, output_size=30*1*768)
             checkpoint = torch.load("MLP_5.pth", map_location=torch.device('cpu'))
             self.model.fc1.weight.data = checkpoint['fc1.weight']
             self.model.fc1.bias.data = checkpoint['fc1.bias']
@@ -28,17 +28,25 @@ class ModelBuilder():
             self.model.fc3.bias.data = checkpoint['fc3.bias']
 
     def return_prediciton(self, prompt_tokens):
-         self.model.eval()
+        self.model.eval()
+        
+        if self.name == "mlp_unpad":
+           tokens = []
+           for i in range(prompt_tokens.size(0)):
+               prompt_token = prompt_tokens[i, :, :]
+               output_token = self.model(prompt_token)
+               tokens.append(output_token)
 
-         if self.name == "mlp_unpad":
-            tokens = []
-            for i in range(prompt_tokens.size(0)):
-                prompt_token = prompt_tokens[i, :, :]
-                output_token = self.model(prompt_token)
-                tokens.append(output_token)
-
-            combined_tokens = torch.stack(tokens, dim=0)
-            return combined_tokens
+           combined_tokens = torch.stack(tokens, dim=0)
+           return combined_tokens
          
-         if self.name == "mlp_pad":
-             return NotImplemented
+        if self.name == "mlp_pad":
+            test_embed = prompt_tokens
+            test_embed = test_embed.flatten()
+            test_embed = test_embed.unsqueeze(dim=0)
+
+            output = self.model(test_embed) #(b, 30*1*768)
+            output = output.squeeze(dim=0)
+            output = output.reshape(30,1,768)              
+            return output
+             
